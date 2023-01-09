bfea68a547834c1e9756b791285d9a843262dd1f
Samuel Ramos <srsantosdev@gmail.com>
2023-01-08 23:14:12 -0300

    first commit

---
+++ src/main/java/pro/samuelramos/ecommerce/NewOrderMain.java
@@ -0,0 +1,45 @@
+package pro.samuelramos.ecommerce;
+
+import org.apache.kafka.clients.producer.Callback;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.util.Properties;
+import java.util.concurrent.ExecutionException;
+
+public class NewOrderMain {
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+        var producer = new KafkaProducer<String, String>(properties());
+        var value = "123123,3453453,34543";
+        var record = new ProducerRecord<String, String>("ECOMMERCE_NEW_ORDER", value, value);
+
+        var email = "Thank you for your order! We are processing the order.";
+        var emailRecord = new ProducerRecord<>("ECOMMERCE_SEND_EMAIL", email, email);
+
+        producer.send(record, callback()).get();
+        producer.send(emailRecord, callback()).get();
+    }
+
+    private static Properties properties() {
+        var properties = new Properties();
+
+        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
+        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+        return properties;
+    }
+
+    private static Callback callback() {
+        return (data, ex) -> {
+            if (ex != null) {
+                ex.printStackTrace();
+                return;
+            }
+
+            System.out.println("[Success] sending: " + data.topic() + " :::partition " + data.partition() + " / offset " + data.offset() + " / timestamp " + data.timestamp());
+        };
+    }
+}
